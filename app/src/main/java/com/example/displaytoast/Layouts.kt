b44980e624fdfd08051071df0030db07c06963be
Shivangi <shivangit1111@gmail.com>
2023-03-10 16:27:56 +0530

    chore : create scrollActivity

--- app/src/main/java/com/example/displaytoast/Layouts.kt
+++ app/src/main/java/com/example/displaytoast/Layouts.kt
@@ -1,11 +1,20 @@
 package com.example.displaytoast
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.displaytoast.databinding.ActivityLayootsBinding
 
 class Layouts : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLayootsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_layoots)
+        binding = ActivityLayootsBinding.inflate(layoutInflater)
+        binding.btn2.setOnClickListener {
+            val intent = Intent(this, ScrollView::class.java)
+            startActivity(intent)
+        }
+        setContentView(binding.root)
     }
 }
